0abbb0a39ce9f88c510c3455b2391e1384449da4
Jojo <zzc185@foxmail.com>
2018-03-26 23:40:39 +0800

    second commit

--- src/SearchBaidu.java
+++ src/SearchBaidu.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
